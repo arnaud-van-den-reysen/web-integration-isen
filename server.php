743a0911c86428e84e3011b1c578597c9aaa5eea
Generationne <steametjeuxonline@hotmail.fr>
2020-07-27 15:02:43 +0200

    enregistrement fonctionnel

--- server.php
+++ server.php
@@ -20,7 +20,7 @@ if (isset($_POST['reg_user'])) {
     // form validation: ensure that the form is correctly filled ...
     // by adding (array_push()) corresponding error unto $errors array
     if (empty($username)) { array_push($errors, "Username is required"); }
-    if (empty($email)) { array_push($errors, "Email is required"); }
+    if (empty($mail)) { array_push($errors, "Email is required"); }
     if (empty($password_1)) { array_push($errors, "Password is required"); }
     if ($password_1 != $password_2) {
         array_push($errors, "The two passwords do not match");
@@ -46,8 +46,8 @@ if (isset($_POST['reg_user'])) {
     if (count($errors) == 0) {
         $password = hash('sha256',$password_1);//encrypt the password before saving in the database
 
-        $query = "INSERT INTO authentication (username, mail, password) 
-  			  VALUES('$username', '$mail', '$password')";
+        $query = "INSERT INTO authentication (username, mail, password, isAdmin, isDoctor) 
+  			  VALUES('$username', '$mail', '$password', 0 , 0)";
         mysqli_query($db, $query);
         $_SESSION['username'] = $username;
         $_SESSION['success'] = "You are now logged in";
