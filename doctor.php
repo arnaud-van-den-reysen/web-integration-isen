a9cb2e78b61de6dc2431d17597cca33d7ee3211c
Arnaud VDR <52168487+arnaud-van-den-reysen@users.noreply.github.com>
2020-08-03 18:55:35 +0200

    doctor appoint details

--- doctor.php
+++ doctor.php
@@ -22,6 +22,42 @@ $db = connectDb();
 
     <!-- Custom styles for this template -->
     <link href="Static/css/cover.css" rel="stylesheet">
+    <style>
+    details {
+        margin-right: 15px;
+        margin-bottom: 15px;
+    }
+    details > summary {
+        padding: 4px;
+        border: none;
+        cursor: pointer;
+        padding-right: 10px;
+        padding-left: 10px;
+        list-style-type: none;
+    }
+
+    details > p {
+        padding: 4px;
+        margin: 0;
+    }
+    .title_doctor {
+        font-size: 40px;
+        
+    }
+    .container {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: flex-start;
+        overflow: auto;
+        position: relative;
+    }
+    .une_belle_boite {
+        border-right-style: solid;
+        border-right-width: 5px;
+        border-color: white;
+        background-image: linear-gradient(to right,#333,#000);
+    }
+    </style>
 
 </head>
 
@@ -52,20 +88,25 @@ $db = connectDb();
     <main role="main" class="inner cover">
        <?php
         $username = $_SESSION['username'];
-         $doctor_display = "SELECT * FROM (authentication INNER JOIN doctor ON authentication.id_auth = doctor.id_auth) INNER JOIN social_details ON social_details.id_socdet = doctor.id_socdet WHERE username='$username'";
-         $result = mysqli_query($db, $doctor_display);
-         $user = mysqli_fetch_assoc($result);
-         echo 'Id_auth :' . $user['id_auth'] . ' First name : ' . $user['firstname'] . ' Last name : ' . $user['lastname'] . ' RPPS : ' . $user['rpps'];
-         echo '<br><br><br>';
-         $id_doct = $user['id_doc'];
-         $appointment_display = "SELECT * FROM (((appointment INNER JOIN patient_medical_record ON appointment.id_medrec = patient_medical_record.id_medrec) INNER JOIN social_details ON social_details.id_socdet = patient_medical_record.id_socdet) INNER JOIN gps_coordinates ON appointment.id_gpsc = gps_coordinates.id_gpsc) INNER JOIN address ON address.id_addr = appointment.id_addr WHERE appointment.id_doc = '$id_doct'";
-         $appointment_res = mysqli_query($db, $appointment_display);
-         while ($row = mysqli_fetch_assoc($appointment_res)) {
-             echo '<br>';
-             echo 'Appointment date : ' . $row['date_appoi'] . ' Appointment Hour : ' . $row['hours_appoi'] . ' Appointment Address :'. $row['number']. ' '. $row['street']. ' '. $row['zipcode'] . ' Patient first name : ' . $row['firstname'] . ' Patient last name : ' . $row['lastname'] . ' Patient Social Security Number : ' . $row['socialsecuritynumber'];
-             echo ' GPS Coordinates  Longitude : ' . $row['longitude'] . ' Latitude : ' . $row['latitude'];
-             echo '<br><br>';
-         }
+        $doctor_display = "SELECT * FROM (authentication INNER JOIN doctor ON authentication.id_auth = doctor.id_auth) INNER JOIN social_details ON social_details.id_socdet = doctor.id_socdet WHERE username='$username'";
+        $result = mysqli_query($db, $doctor_display);
+        $user = mysqli_fetch_assoc($result);
+        echo '<details>';
+        echo '<summary class="title_doctor"> Docteur ' . $user['firstname'] . ' ' . $user['lastname'] . '</summary>';
+        echo '<p> ID : ' . $user['id_auth'] . ' RPPS : ' . $user['rpps'] . '</p>';
+        echo '</details>';
+        $id_doct = $user['id_doc'];
+        $appointment_display = "SELECT * FROM (((appointment INNER JOIN patient_medical_record ON appointment.id_medrec = patient_medical_record.id_medrec) INNER JOIN social_details ON social_details.id_socdet = patient_medical_record.id_socdet) INNER JOIN gps_coordinates ON appointment.id_gpsc = gps_coordinates.id_gpsc) INNER JOIN address ON address.id_addr = appointment.id_addr WHERE appointment.id_doc = '$id_doct' ORDER BY appointment.date_appoi";
+        $appointment_res = mysqli_query($db, $appointment_display);
+        echo '<div class="container">';
+        while ($row = mysqli_fetch_assoc($appointment_res)) {
+            echo '<details>';
+            echo '<summary class="une_belle_boite">' . $row['date_appoi'] . '</summary>';
+            echo '<p>Appointment date : ' . $row['date_appoi'] . ' Appointment Hour : ' . $row['hours_appoi'] . ' Appointment Address :'. $row['number']. ' '. $row['street']. ' '. $row['zipcode'] . ' Patient first name : ' . $row['firstname'] . ' Patient last name : ' . $row['lastname'] . ' Patient Social Security Number : ' . $row['socialsecuritynumber'];
+            echo ' GPS Coordinates  Longitude : ' . $row['longitude'] . ' Latitude : ' . $row['latitude'] . '</p>';
+            echo '</details>';
+        }
+        echo '</div>';
        ?>
 
 
