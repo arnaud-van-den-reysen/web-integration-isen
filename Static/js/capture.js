8de2efa3c4b6e1007c376728bc86d1d93bae2d9d
Arnaud VDR <52168487+arnaud-van-den-reysen@users.noreply.github.com>
2020-09-03 18:29:03 +0200

    Save video 0.1

--- Static/js/capture.js
+++ Static/js/capture.js
@@ -14,13 +14,66 @@ function afterLoaded() {
     //  to delay more, or use a MutationObserver to see when they are inserted.
     let preview = document.getElementById("preview");
 
+    let recordingTimeMS = 5000;
+
     if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({
             video: true,
             audio: false
         }).then(stream => {
             preview.srcObject = stream;
-            preview.captureStream = preview.captureStream || preview.mozCaptureStream;
+            preview.captureStream = preview.captureStream || preview.mozCaptureStream
+            return new Promise(resolve => preview.onplaying = resolve);
+        }).then(() => startRecording(preview.captureStream(), recordingTimeMS))
+        .then (recordedChunks => {
+            let recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
+            
+            let formData = new FormData();
+            formData.append('video', recordedBlob);
+            //fetch('videos', {method: "POST", headers: { "Content-type": "video/webm" }, body: formData});
+
+            xhr('videos/php/upload_video.php', formData, function (fName) {
+                console.log("Video succesfully uploaded !");
+            });
+
+            function xhr(url, data, callback) {
+                var request = new XMLHttpRequest();
+                request.onreadystatechange = function () {
+                    if (request.readyState == 4 && request.status == 200) {
+                        callback(location.href + request.responseText);
+                    }
+                };
+                request.open('POST', url);
+                request.send(data);
+            }
+            console.log("UPLOAD BORDEL!")
         })
     };
+}
+
+function startRecording(stream, lengthInMS) {
+    let recorder = new MediaRecorder(stream);
+    let data = [];
+
+    recorder.ondataavailable = event => data.push(event.data);
+    recorder.start();
+
+    let stopped = new Promise((resolve, reject) => {
+        recorder.onstop = resolve;
+        recorder.onerror = event => reject(event.name);
+    });
+
+    let recorded = wait(lengthInMS).then(
+        () => recorder.state == "recording" && recorder.stop()
+    );
+
+    return Promise.all([
+        stopped,
+        recorded
+    ])
+    .then(() => data);
+}
+
+function wait(delayInMS) {
+    return new Promise(resolve => setTimeout(resolve, delayInMS));
 }
